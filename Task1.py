a444159fd4ff08c1a47bbbc0cf2185875e4150d6
RashaAbuSamhan <96924603+RashaAbuSamhan@users.noreply.github.com>
2022-08-20 22:13:19 +0300

    Add files via upload

---
+++ Task1.py
@@ -0,0 +1,12 @@
+print ("Enter 8 people's names of your choice")
+name1 = input ("enter name1:")
+name2 = input ("enter name2:")
+name3 = input ("enter name3:")
+name4 = input ("enter name4:")
+name5 = input ("enter name5:")
+name6 = input ("enter name6:")
+name7 = input ("enter name7:")
+name8 = input ("enter name8:")
+names = [name1,name2,name3,name4,name5,name6,name7,name8,]
+import random
+print(random.choice(names))
